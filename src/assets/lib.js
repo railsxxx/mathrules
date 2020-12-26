2ba7a872ea5c9b8bc7a0ae229c7945877fb8d0db
railsxxx <59250712+railsxxx@users.noreply.github.com>
2020-12-26 09:21:06 +0100

    import lib.js focusMQ

--- src/assets/lib.js
+++ src/assets/lib.js
@@ -1,45 +1,17 @@
-// const arrMQ = [];
-let active = undefined;
-let isActive = false;
-// function reg(mq) {
-//   if (!arrMQ.includes(mq)) arrMQ.push(mq);
-//   console.log("reg: mq: ", mq);
-//   console.log("reg: active: ", active);
-//   console.log("reg: arrMQ: ", arrMQ);
-// }
-// function unreg(mq) {
-//   const index = arrMQ.indexOf(mq);
-//   if (index >= 0) arrMQ.splice(index, 1);
-// }
-function clicked(mq) {
-  console.log("clicked: ", mq);
-  if (active) active.blur();
-  active = mq;
-  isActive = true;
-  // arrMQ.forEach((item) => {
-  //   if (item === mq) item.focus();
-  //   else item.blur();
-  // });
+export function focusMQ() {
+  let mq = undefined;
+  return {
+    set: function (mqn) {
+      // console.log("clicked: ", mqn);
+      if (mq) mq.blur();
+      mq = mqn;
+    },
+    get: function () {
+      return mq;
+    },
+    clear: function () {
+      if (mq) mq.blur();
+      mq = undefined;
+    }
+  };
 }
-// function get() {
-//   return active;
-// }
-// function clear(mq) {
-//   if (active === mq) {
-//     active = {};
-//     isActive = false;
-//   }
-// }
-// function isFocus() {
-//   return isActive;
-// }
-
-export default {
-  // register: reg,
-  // unregister: unreg,
-  set: clicked,
-  // get: get,
-  // clear: clear,
-  isAny: isActive,
-  mq: active
-};
--- src/main.js
+++ src/main.js
@@ -4,28 +4,30 @@ import { createApp, ref } from "vue";
 import App from "./App.vue";
 import "./assets/reset.css";
 
-import focusMQ from "./assets/lib.js";
+import { focusMQ } from "./assets/lib.js";
 
 const app = createApp(App);
 
 app.config.globalProperties.gClipboard = "clip";
 app.config.globalProperties.gFocusMQref = ref({});
-app.config.globalProperties.gFocusMQobj = (function () {
-  let mq = undefined;
-  return {
-    set: function (mqn) {
-      // console.log("clicked: ", mqn);
-      if (mq) mq.blur();
-      mq = mqn;
-    },
-    get: function () {
-      return mq;
-    },
-    clear: function () {
-      if (mq) mq.blur();
-      mq = undefined;
-    }
-  };
-})();
+app.config.globalProperties.gFocusMQobj = focusMQ();
+
+// (function () {
+//   let mq = undefined;
+//   return {
+//     set: function (mqn) {
+//       // console.log("clicked: ", mqn);
+//       if (mq) mq.blur();
+//       mq = mqn;
+//     },
+//     get: function () {
+//       return mq;
+//     },
+//     clear: function () {
+//       if (mq) mq.blur();
+//       mq = undefined;
+//     }
+//   };
+// })();
 
 app.mount("#app");
